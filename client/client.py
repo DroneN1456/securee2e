2ce53aea8c30589115c049572c01b4227959c4ac
DroneN1456 <gugagrilo07@gmail.com>
2023-01-21 20:38:36 -0300

    1.0.0

--- client/client.py
+++ client/client.py
@@ -1,9 +1,18 @@
-import socket
-from threading import Thread
 from datetime import datetime
+from threading import Thread
+from termcolor import colored
+from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 5002
+
+import socket
+
+SERVER_HOST = input('Enter server ip: ')
+SERVER_PORT = int(input('Enter server port: '))
+print()
 
 s = socket.socket()
 print(f'[*] Connecting to {SERVER_HOST}:{SERVER_PORT}')
@@ -11,12 +20,24 @@ s.connect((SERVER_HOST, SERVER_PORT))
 print('[+] Connected')
 
 name = input('Enter your username: ')
+password_provided = input('Please inform your decryption key: ').encode()
+salt = b'salt_'
+kdf = PBKDF2HMAC(algorithm=hashes.SHA256, salt=salt,
+                 iterations=1000, backend=default_backend(), length=32)
+key = base64.urlsafe_b64encode(kdf.derive(password_provided))
+f = Fernet(key)
 
 
 def listen_for_messages():
+    global f
     while True:
-        message = s.recv(1024).decode()
-        print('\n' + message)
+        message = s.recv(1024)
+        try:
+            message = f.decrypt(message)
+            print(message.decode())
+        except Exception as e:
+            print(colored(f'[!] Error: {e}'))
+            exit()
 
 
 t = Thread(target=listen_for_messages)
@@ -24,12 +45,13 @@ t.daemon = True
 t.start()
 
 while True:
-    to_send = input()
-
-    if to_send.lower() == 'q':
+    msg = input()
+    if (msg == 'q'):
         break
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    to_send = f'[{name}]: {to_send}'
-    s.send(to_send.encode())
+    print("\033[A                             \033[A")
+    msg = f'{name}: {msg}'
+    msg = f.encrypt(msg.encode())
+    s.send(msg)
+
 
 s.close()
--- server/server.py
+++ server/server.py
@@ -1,36 +1,68 @@
 import socket
+import os
+import platform
+import configparser
+from termcolor import colored
 from threading import Thread
 
-SERVER_HOST = '0.0.0.0'
-SERVER_PORT = 5002
+version = '1.0.0'
 
-clients = set()
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((SERVER_HOST, SERVER_PORT))
-
-s.listen(5)
-print(f'[*] Listening on {SERVER_HOST}:{SERVER_PORT}')
+SERVER_HOST = str(config['GENERAL']['SERVER_HOST'])
+SERVER_PORT = int(config['GENERAL']['SERVER_PORT'])
 
 
-def listen_clients(cs):
+def clear_console():
+    system = platform.system()
+    if (not system == 'Windows'):
+        os.system('clear')
+    else:
+        os.system('cls')
+
+
+def main():
+    print(colored(f'Welcome to SecureE2E {version}!'))
+    print()
+    print(colored('[1] Start Server'))
+    response = int(input())
+    if response == 1:
+        start_server()
+
+
+def start_server():
+    client_sockets = set()
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((SERVER_HOST, SERVER_PORT))
+    s.listen(5)
+    clear_console()
+    print(colored('[*] ', 'yellow') +
+          f'Listening to {SERVER_HOST}:{SERVER_PORT}')
+
+    while True:
+        client_socket, client_addr = s.accept()
+        print(colored('[+] ', 'green') + f'{client_addr} connected')
+        client_sockets.add(client_socket)
+        t = Thread(target=listen_to_clients, args=(
+            client_socket, client_sockets))
+        t.daemon = True
+        t.start()
+
+    for cs in client_sockets:
+        cs.close()
+    s.close()
+
+
+def listen_to_clients(cs, clients):
     while True:
         try:
             msg = cs.recv(1024).decode()
         except Exception as e:
-            print(f'[!] Client Desconnected')
-            clients.remove(cs)
+            print(colored('[!] ', 'red') + f'{e}')
         for client in clients:
             client.send(msg.encode())
 
 
-while True:
-    client, client_addr = s.accept()
-    print([f'[+] {client_addr} connected'])
-    clients.add(client)
-
-    t = Thread(target=listen_clients, args=(client,))
-
-    t.daemon = True
-    t.start()
+main()
